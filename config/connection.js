c4df65102af20b5e140cf6cf557b581823ea50d5
Dono <erik.dono@gmail.com>
2020-05-27 20:20:55 -0400

    adding jaws

--- config/connection.js
+++ config/connection.js
@@ -4,22 +4,22 @@ require('dotenv').config()
 const dbpass = process.env.password
 console.log(dbpass)
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "b4e9xxkxnpu2v96i.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: dbpass,
-    database: "burgerDB"
+    user: "nt0awbeh06ry5ejs",
+    password: "e6g62kym9vlt748m",
+    database: "c241uhxczjumz6gb"
 });
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        passoword: dbpass,
-        database: "burgerdb"
-    })
-}
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         passoword: dbpass,
+//         database: "burgerdb"
+//     })
+// }
 
 // Make connection.
 connection.connect(function (err) {
